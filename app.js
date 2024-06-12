13ce7a4e3860f42703a3c23cacbf488e2f32d4b0
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-12 16:05:55 -0400

    Version 3

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import { sequelize } from './database/database.js'; // Asegúrate de importar esto correctamente
 import medicRoutes from './routes/medic.routes.js'; // Usa la nueva ruta
+import dotenv from 'dotenv';
+
+dotenv.config(); // Cargar variables de entorno desde .env
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Usar el puerto definido en las variables de entorno, o 3000 por defecto
 
 // Middleware
 app.use(morgan('dev'));
--- database/database.js
+++ database/database.js
@@ -1,12 +1,16 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Cargar variables de entorno desde .env
 
 export const sequelize = new Sequelize(
-  "medic", // db name
-  "postgres", // username
-  "12345678", // password
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: "localhost",
-    dialect: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Utilizar el puerto definido en las variables de entorno
+    dialect: 'postgres',
     pool: {
       max: 5,
       min: 0,
